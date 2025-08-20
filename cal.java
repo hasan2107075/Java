6793678fc1ee8520c9c7317cc9cd189a48a904f8
Rezuan Mustafa Hasan <hasan2107075@stud.kuet.ac.bd>
2025-08-20 03:50:56 -0700

    rand1

---
+++ cal.java
@@ -0,0 +1,14 @@
+public class Calculator {
+  private String NAME; // private = restricted access
+
+  // Getter
+  public String getName() {
+    return name;
+  }
+
+  // Setter
+  public void setName(String newName) {
+    this.name = newName;
+    
+  }
+}
